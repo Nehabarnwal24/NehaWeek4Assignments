31bbe137b4199a1535a9990289e42e32aae97a3e
Nehabarnwal24 <nehabarnwal9794@gmail.com>
2026-04-02 15:08:59 +0530

    Files Added

---
+++ April1Assignments/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Storage.Blobs;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static string str_connection = "DefaultEndpointsProtocol=https;AccountName=nehastoragecg;AccountKey=w9iJidbSSTD2UPvCR5QnIMA0XJ0tYSZaXhmbhHLEltTy2Yww5lPmcRJQiQEvSHp7hbZXqgq/hrB++ASths4arg==;EndpointSuffix=core.windows.net";// this value i had kept empty u have to take it from azure access keys and add it and in github u cant put sececet 
+        // connection like this becasue when u try to commit it tells secret information is there so for security reasong i cannot do commit so removed
+
+        static async Task Main()
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(str_connection);
+            BlobContainerClient container_client =
+            blobServiceClient.GetBlobContainerClient("data");
+            BlobClient blob_client = container_client.GetBlobClient("myFile.txt");
+            using FileStream uploadFileStream = File.OpenRead(@"C:\Users\91979\OneDrive\Desktop\Text files\myFile.txt");
+            await blob_client.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+            Console.WriteLine("File uploaded");
+            Console.WriteLine("Operation complete");
+        }
+    }
+}
